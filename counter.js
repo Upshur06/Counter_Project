6971c03e2d99dd0c9f721512f8c856f65751bdf8
Upshur06 <mupshur2005@gmail.com>
2020-08-11 01:25:22 -0400

    color change

--- counter.js
+++ counter.js
@@ -13,6 +13,13 @@ btns.forEach(function(btn){
         } else{
           count = 0;
         }
+          if(count < 0){
+            value.style.color= "red";
+          }else if(count > 0){
+            value.style.color="green";
+          }else{
+            value.style.color="#222"
+          }
         value.textContent = count;
     })
 });
